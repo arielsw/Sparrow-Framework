95a08f7c8f057818860e3b3ff73b1ace900c4217
Ariel <ariel.skunkwerks.ca>
2013-05-31 20:47:36 -0700

    -Created an ISPBaseEffectTexture interface

---
+++ sparrow/src/Classes/ISPBaseEffectTexture.h
@@ -0,0 +1,13 @@
+//
+//  ISPBaseEffectTexture.h
+//  Sparrow
+//
+//  Created by Ariel on 2013-05-31.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ISPBaseEffectTexture <NSObject>
+
+@end
