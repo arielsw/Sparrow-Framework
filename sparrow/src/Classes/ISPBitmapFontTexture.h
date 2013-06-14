878eee68c603d200d29ef113fc7a96594c13813d
Ariel <ariel.skunkwerks.ca>
2013-06-14 16:11:31 -0700

    -Created the ISPBitmapFontTexture interface

---
+++ sparrow/src/Classes/ISPBitmapFontTexture.h
@@ -0,0 +1,13 @@
+//
+//  ISPBitmapFontTexture.h
+//  Sparrow
+//
+//  Created by Ariel on 2013-06-14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ISPBitmapFontTexture <NSObject>
+
+@end
