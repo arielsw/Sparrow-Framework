dc8f328fe27b0b98285c4857a9db84c2d3426fa5
Ariel <ariel.skunkwerks.ca>
2013-06-17 10:43:18 -0700

    -Created the ISPTextureAtlasTexture interface

---
+++ sparrow/src/Classes/ISPTextureAtlasTexture.h
@@ -0,0 +1,13 @@
+//
+//  ISPTextureAtlasTexture.h
+//  Sparrow
+//
+//  Created by Ariel on 2013-06-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ISPTextureAtlasTexture <NSObject>
+
+@end
