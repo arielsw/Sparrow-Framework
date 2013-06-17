436dbaa64b55440f433480b51b84daed8e4c1693
Ariel <ariel.skunkwerks.ca>
2013-06-17 10:51:09 -0700

    -Created the ISPSubTextureTexture interface

---
+++ sparrow/src/Classes/ISPSubTextureTexture.h
@@ -0,0 +1,13 @@
+//
+//  ISPSubTextureTexture.h
+//  Sparrow
+//
+//  Created by Ariel on 2013-06-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ISPSubTextureTexture <NSObject>
+
+@end
